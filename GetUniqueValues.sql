fafa809864a78961245f193b9392cedfcf7bead8
Claire Njeri <clairemwas20@gmail.com>
2025-12-26 10:18:27 +0300

    Get UNIQUE values

---
+++ GetUniqueValues.sql
@@ -0,0 +1,9 @@
+USE AdventureWorksDW2025
+
+SELECT DISTINCT Color FROM DimProduct
+
+SELECT Color,ListPrice FROM DimProduct
+
+SELECT DISTINCT Color,ListPrice FROM DimProduct
+
+SELECT DISTINCT * FROM DimProduct
